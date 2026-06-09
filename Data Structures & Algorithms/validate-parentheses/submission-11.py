16e38109324ab29a8bc6619a49dee9da325b5590
tomieP <nhphuc282@gmail.com>
2026-06-09 15:39:20 +0700

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st = []
+        h = {
+            '}':'{',
+            ')':'(',
+            ']':'['
+        }
+        if len(s) <= 1: return False
+        for c in s:
+            if c in h:
+                if st and st[-1] == h[c]:
+                        st.pop()
+                else: return False
+            else:
+                st.append(c)
+        return True if not st else False
